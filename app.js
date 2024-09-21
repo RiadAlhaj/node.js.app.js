572ad7772fa0e7514298d7d8a47bb14b72ca2bf6
Riad Alhaj <157020742+RiadAlhaj@users.noreply.github.com>
2024-09-21 15:49:02 +0200

    add app.js file
    add the app.js file after trying node on the code editor terminal.

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("This is my first node.js program");
+console.log("Start of the program");
+
+setTimeout (()=>{
+  console.log("this message appears after 3 seconds");
+},3000);
+console.log("End of the program");
+
